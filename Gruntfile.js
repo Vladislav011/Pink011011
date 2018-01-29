9093e6d492366f75e0271f658000f1e92572d94a
Your Name <you@example.com>
2018-01-29 23:57:51 +0300

    end2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,25 @@
 'use strict'
 module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-autoprefixer");
   grunt.initConfig({
+    autoprefixer : {
+      options : {
+      browsers: ['last 2 versions', 'ie 8', 'ie 9','opera 12', 'ff 15', 'chrome 25','safari >= 9','safari 8']
+      } ,
+      your_target : {
+        src: 'css/style.css',
+      } ,
+    } ,
 less:{
   style:{
+
+
     files:{
       "css/style.css":"less/style.less"
     }
   }
+
 
 
 }
